76e04e6d9bec5e40ee0f64a857d68ba7778b6848
Vectormike40 <victor.junior40@yahoo.com>
2019-06-24 15:39:09 +0100

    ...

---
+++ Members.js
@@ -0,0 +1,14 @@
+const members = [
+    {name: 'John Doe', age: 22},
+    {name: 'June Smith', age: 23},
+    {name: 'Kim Max', age: 12},
+    {name: 'John Mike', age: 43},
+    {name: 'David Doe', age: 45},
+    {name: 'Jon Doe', age: 55},
+    {name: 'Smalls Jane', age: 32},
+    {name: 'David Luke', age: 54},
+    {name: 'Collins Doe', age: 33},
+    {name: 'John Smith', age: 11}
+]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express');
+const path = require('path');
+const logger = require('./Middlewares/logger');
+const members = require('./Members');
 const app = express();
+
+
+// Init middleware
+app.use(logger);
+
+// Get All Members
+app.get('/api/members', (req, res) => {
+    res.json(members);
+})
+
+
+
+// Set static folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
